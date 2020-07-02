0efd8a5ce666ed7f5a9d8ec09b881de433af035e
playerplayer210 <59975613+playerplayer210@users.noreply.github.com>
2020-07-02 20:37:22 +0300

    New Project

---
+++ en/projects/SquMove/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1593711291,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/scrollmarker-sheet0.png",
+		"images/particles.png",
+		"images/death-sheet0.png",
+		"images/spec-sheet0.png",
+		"images/trail-sheet0.png",
+		"images/solid-sheet0.png",
+		"images/deathobj-sheet0.png",
+		"images/deathobj-sheet1.png",
+		"images/zone-sheet0.png",
+		"images/loading_logo-sheet0.png",
+		"images/light-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
