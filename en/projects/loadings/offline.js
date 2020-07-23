e021b94d0db0647b68f33d5102afd525e7dfbdaa
playerplayer210 <59975613+playerplayer210@users.noreply.github.com>
2020-07-23 18:59:06 +0300

    Added new project

---
+++ en/projects/loadings/offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1595519874,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/loading_bar1-sheet0.png",
+		"images/loading_bar3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
